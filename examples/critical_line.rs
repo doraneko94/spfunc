e0bd49b69138d024b8fc2797ff558baed8cb60a8
Shuntaro Ohno <sohno@ushitora.net>
2025-07-11 13:34:54 +0900

    add fig refs #2

--- examples/critical_line.rs
+++ examples/critical_line.rs
@@ -6,22 +6,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let t_start = 765.0;
     let t_end = 770.0;
     let t_step = 0.01;
-    let epsilon = 1e-6;
+    let epsilon = 1e-4;
 
     let root = BitMapBackend::new("zeta_critical_line.png", (1200, 800)).into_drawing_area();
     root.fill(&WHITE)?;
 
     // Scale plotting area
     let mut chart = ChartBuilder::on(&root)
-        .caption("Riemann Zeta on Critical Line", ("sans-serif", 25))
+        .caption("Rust: spfunc::zeta::zeta", ("sans-serif", 25))
         .margin(20)
         .x_label_area_size(40)
         .y_label_area_size(40)
         .build_cartesian_2d(t_start..t_end, -1.5f64..3.0f64)?;
 
     chart.configure_mesh()
-        .x_desc("t in s = 0.5 + i·t")
-        .y_desc("Re / Im / zero detector")
+        .x_desc("0.5 + xi")
+        .y_desc("y")
         .draw()?;
 
     let mut samples_re = Vec::new();
@@ -38,23 +38,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         // Spike |ζ(s)| is nearly zero.
         let norm_sq = z.re * z.re + z.im * z.im;
-        if norm_sq < epsilon * epsilon {
-            zero_spikes.push(vec![(t, -30.0), (t, 30.0)]);
+        if norm_sq < epsilon {
+            // zero_spikes.push(vec![(t, -30.0), (t, 30.0)]);
+            zero_spikes.push((t, 1.0))
+        } else {
+            zero_spikes.push((t, 0.0))
         }
     }
 
     // Plot
    chart.draw_series(LineSeries::new(samples_re, &BLUE))?
-        .label("Re(ζ(0.5 + i·t))")
+        .label("real")
         .legend(|(x, y)| PathElement::new([(x, y), (x + 20, y)], &BLUE));
 
     chart.draw_series(LineSeries::new(samples_im, &RED))?
-        .label("Im(ζ(0.5 + i·t))")
+        .label("image")
         .legend(|(x, y)| PathElement::new([(x, y), (x + 20, y)], &RED));
 
-    for spike in zero_spikes {
-        chart.draw_series(LineSeries::new(spike, &GREEN))?;
-    }
+    //for spike in zero_spikes {
+    //    chart.draw_series(LineSeries::new(spike, &GREEN))?;
+    //}
+    chart.draw_series(LineSeries::new(zero_spikes, &GREEN))?
+        .label("detector")
+        .legend(|(x, y)| PathElement::new([(x, y), (x + 20, y)], &GREEN));
+
 
     chart.configure_series_labels().border_style(&BLACK).draw()?;
 
